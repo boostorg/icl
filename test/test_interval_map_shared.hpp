62d7c98db143b194ae683b74bb26085c54d48714
joaquintides <joaquin@tid.es>
2026-03-22 19:48:20 +0100

    removed test workaround after previous fix

--- test/test_interval_map_shared.hpp
+++ test/test_interval_map_shared.hpp
@@ -1247,13 +1247,7 @@ void interval_map_range_4_bicremental_types()
     //    [2        7) := itv
 
     IntervalT itv = I_D(2, 7);
-#if 0
-    // There does not seem to be a guarantee that 
-    // icl::find(map_a, itv) == map_a.lower_bound(itv); ask Joachim Faulhaber about it.
     c_iterator lwb1 = icl::find(map_a, itv);
-#else
-    c_iterator lwb1 = map_a.lower_bound(itv);
-#endif
     c_iterator lwb2 = map_a.lower_bound(itv);
 
     BOOST_CHECK      ( lwb1 == lwb2 );
